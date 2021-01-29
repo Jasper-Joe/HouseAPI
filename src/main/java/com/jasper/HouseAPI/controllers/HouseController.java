ef9a222e63503869b2888e651debd1177a69b3cd
jasper <jasperzhou1129@gmail.com>
2021-01-29 14:33:22 -0500

    error handling

--- src/main/java/com/jasper/HouseAPI/controllers/HouseController.java
+++ src/main/java/com/jasper/HouseAPI/controllers/HouseController.java
@@ -1,8 +1,16 @@
 package com.jasper.HouseAPI.controllers;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +27,15 @@ public class HouseController {
 	private HouseService houseService;
 	
 	@PostMapping("")
-	public ResponseEntity<House> createNewHouse(@RequestBody House house) {
+	public ResponseEntity<?> createNewHouse(@Valid @RequestBody House house, BindingResult result) {
+		if(result.hasErrors()) {
+			Map<String, String> errorMap = new HashMap<>();
+			for(FieldError error: result.getFieldErrors()) {
+				errorMap.put(error.getField(), error.getDefaultMessage());
+			}
+			
+			return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
+		}
 		House newHouse = houseService.saveOrUpdateHouse(house);
 		return new ResponseEntity<House>(house, HttpStatus.CREATED);
 	}
