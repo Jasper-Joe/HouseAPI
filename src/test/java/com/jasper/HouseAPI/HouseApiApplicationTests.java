0678cd68851065c8b9ee70a1063e3580d45c3b7c
jasper <jasperzhou1129@gmail.com>
2021-01-29 12:30:11 -0500

    first commit

---
+++ src/test/java/com/jasper/HouseAPI/HouseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasper.HouseAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
